3ae059c3172e9b9df0dcb8c0c3e557a1de07ded8
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-05-21 20:37:02 +0100

    styled the navigation bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo_small from '../assets/icons/logo-small.png';
 import logo_letter from '../assets/icons/logo-letter.png';
+import './nav.css';
 
 
 const Nav = (props) => {
   return (
     <div className="Nav">
       <div className="logo">
-        <img src={ logo_small } alt="" />
+        <img className="logo-icon" src={ logo_small } alt="" />
         <img src={ logo_letter } alt="" />
       </div>
       <div className="nav-links">
         <ul>
           <div className="left-nav-links">
-            <Link to="/our-team"><li>Our team</li></Link>
-            <Link to="/tokenomics"><li>Tokens</li></Link>
-            <Link to="/connect-wallet"><li>Connect wallet</li></Link>
-            <Link to="/home"><li>Lightpaper</li></Link>
+            <Link className="anchor-link" to="/our-team"><li>Our team</li></Link>
+            <Link className="anchor-link" to="/tokenomics"><li>Tokens</li></Link>
+            <Link className="anchor-link" to="/connect-wallet"><li>Connect wallet</li></Link>
+            <Link className="anchor-link" to="/home"><li>Lightpaper</li></Link>
           </div>
           
           <div className="right-nav-links">
